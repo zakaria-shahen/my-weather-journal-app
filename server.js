acc559574576c716eddb3b93a4cc446705a93164
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-08-12 22:20:49 +0200

    add code side clint and fix server side

--- server.js
+++ server.js
@@ -2,51 +2,81 @@
 // Express to run server and routes
 const express = require('express');
 const app = express();
-
+const fetch = require('node-fetch');
 // Start up an instance of app
 const port = 9999;
 const server = app.listen(port, () => {
     console.log('Start server - Post ' + port);
 });
-
 /* Dependencies */
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 /* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
 // Initialize the main project folder
 app.use(express.static('website'));
-
 // Spin up the server
 
-
-
 // Callback to debug
-let projectData = [];
+const projectData = {
+    'codeZIP': 0,
+    'feeling': '',
+    'output': {
+        'temp': 0,
+        'date': 0,
+        'content': 0
+    }
+};
 
 
-const keyAPI = '&appid=b44a2e29455b4a07a5b233954a0e112e';
-let linkAPI = 'https://pro.openweathermap.org/data/2.5/forecast/climate?zip=';
+
+// API Weatherapi - using
+const keyAPI = '9992788cb97b473b9ea193321211008';
+const linkAPI = `https://api.weatherapi.com/v1/forecast.json?key=${keyAPI}&days=3&aqi=no&alerts=no&q=`;
+
+
+// fetch data api
+const queryAPI = async(codeZIP) => {
+    try {
+        const newDate = await fetch(linkAPI + codeZIP);
+        return await newDate.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 
 // Initialize all route with a callback function
-app.get('/all', getData);
-app.post('/check', postCheck);
+app.get('/route', (req, res) => {
+    queryAPI(projectData.codeZIP).then((output) => {
+        projectData.output.temp = output.forecast.forecastday[0].day.avgtemp_c;
+        projectData.output.content = output.forecast.forecastday[0].day.condition.text;
+        projectData.output.date = output.forecast.forecastday[0].date;
+        console.log(projectData);
+        res.send(projectData);
+
+    }).catch(() => console.log('get /route: Error'));
+
+});
+
 
 // Callback function to complete GET '/all'
-const getData = (req, res) => {
-    const dataSend = projectData.join();
-    res.send(dataSend);
-};
 
 
-const postCheck = (req, res) => {
+app.post('/sendCodeZIP', (req, res) => {
     const data = req.body;
-    projectData.push(data.temperature, date.date, date.UserResponse);
-};
-
+    projectData.codeZIP = data.codeZIP;
+    projectData.feeling = data.feeling;
+    console.log(data.codeZIP);
+    res.send(projectData);
+});
 
 // Post Route
+
+
+
+// API OpenWeatherMap - NOT using
+// const keyAPI = '&appid=b44a2e29455b4a07a5b233954a0e112e';
+// let linkAPI = 'https://pro.openweathermap.org/data/2.5/forecast/climate?zip=';
--- website/app.js
+++ website/app.js
@@ -1,5 +1,63 @@
 /* Global Variables */
+const button = document.querySelector('#generate');
+const zip = document.querySelector('#zip');
+const feelings = document.querySelector('#feelings');
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const content = document.querySelector('#content');
+
+// let dataClintSide = {};
+
+// get Date output;
+const getData = async(url) => {
+    const res = await fetch(url);
+    try {
+        dataClintSide = await res.json().then(showTemp);
+    } catch (err) {
+        console.log('getData myError: ' + err);
+    }
+
+};
+
+
+const queryWeather = async(dataQuery) => {
+    const res = await fetch('/sendCodeZIP', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataQuery)
+    });
+
+    try {
+
+    } catch (err) {
+        console.log(err);
+
+    }
+};
+
+
+// add Event Listener button click 
+generate.addEventListener('click', () => {
+    let dataInput = { 'codeZIP': 0, 'feelings': '' };
+    dataInput.codeZIP = zip.value;
+    dataInput.feelings = zip.feelings;
+
+    // data Testing: 85001 - hot
+
+    queryWeather(dataInput) // 
+        .then(getData('/route'))
+        .catch((err) => console.log(err));
+
+});
+
+// function showTemp(date, temp, content) {
+function showTemp(dataClintSide) {
+    console.log(dataClintSide);
+    temp.textContent = dataClintSide.output.temp;
+    date.textContent = dataClintSide.output.date;
+    content.textContent = dataClintSide.output.content;
+}
