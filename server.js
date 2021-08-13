0f90ef8285610076a4f68f5b37a22b2f4a20d119
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-08-13 04:37:55 +0200

    fix

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ let projectData = {};
 // Express to run server and routes
 const express = require('express');
 const app = express();
-const fetch = require('node-fetch');
 
 // Start up an instance of app
 const port = 9999;
@@ -30,60 +29,33 @@ app.use(cors());
 app.use(express.static('website'));
 
 
-
-
-
-
 // ------------------ Start code ---------
 
-// API Weatherapi - using
-// const keyAPI = process.env.keyAPI_OpenWeatherMap;
-const keyAPI = 'b44a2e29455b4a07a5b233954a0e112e';
 
 // save code zip request (POST)
-app.post('/sendCodeZIP', (req, res) => {
+app.post('/saveDate', (req, res) => {
     const data = req.body; // get data request
 
     // update projectDate object
     projectData = data;
 
-    res.send(projectData);
-
 });
-
-// fetch data API
-const queryAPI = async(codeZIP, key) => {
-    try {
-        const linkAPI = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=imperial&zip=`;
-        const newDate = await fetch(linkAPI + codeZIP);
-        return await newDate.json();
-    } catch (err) {
-        console.log(err);
-    }
-}
 
 // Initialize all route with a callback function
 app.get('/all', sendData);
 
 // Callback function to complete GET '/all'
 function sendData(req, res) {
-    queryAPI(projectData.codeZIP, keyAPI).then((output) => {
-        // update project.output object 
-        projectData.temp = output.main.temp;
-        // projectData.content = output.weather[0].description;
+    // .then((output) => {
 
-        // get date
-        let d = new Date();
-        projectData.date = +d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
-        // send result 
-        res.send(projectData);
+    //     // // update project.output object 
+    //     // projectData.temp = output.main.temp;
 
-    }).catch((err) => console.log('get /route: Error', err));
+
+    //     // send result 
+    //     res.send(projectData);
+
+    // }).catch((err) => console.log('get /route: Error', err));
+
+    res.send(projectData);
 }
-
-
-
-
-// API OpenWeatherMap - NOT using (not for free)
-// const keyAPI = process.env.keyAPI_OpenWeatherMap;
-// let linkAPI = 'https://pro.openweathermap.org/data/2.5/forecast/climate?zip=';
--- website/app.js
+++ website/app.js
@@ -7,9 +7,31 @@ const date = document.querySelector('#date');
 const temp = document.querySelector('#temp');
 const content = document.querySelector('#content');
 
+// API OpenWeatherMap - NOT using (not for free)
+const keyAPI = 'b44a2e29455b4a07a5b233954a0e112e';
+
+// Get date
+let d = new Date();
+let date_now = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear();
+
+
+
+// fetch data API
+const queryAPI = async(codeZIP, key) => {
+    try {
+        const linkAPI = `https://api.openweathermap.org/data/2.5/weather?appid=${key}&units=imperial&zip=`;
+        const newDate = await fetch(linkAPI + codeZIP);
+        return await newDate.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+
 
 // query save zip code in server side (POST)
-const queryWeather = async(dataQuery, url) => {
+const save = async(dataQuery, url) => {
     const res = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
@@ -20,7 +42,7 @@ const queryWeather = async(dataQuery, url) => {
     });
 
     try {
-
+        const temp = await res.body.json();
     } catch (err) {
         console.log(err);
 
@@ -43,7 +65,7 @@ const getData = async() => {
 // add Event Listener button click 
 generate.addEventListener('click', () => {
     // object save data
-    let dataInput = { 'codeZIP': 0, 'feelings': '' };
+    let dataInput = { 'codeZIP': 0, 'feelings': '', 'date': date_now, 'temp': 0 };
 
     // Check Filed Not Empty
     if (zip.value.length !== 0 && feelings.value.length !== 0) {
@@ -51,10 +73,14 @@ generate.addEventListener('click', () => {
         dataInput.codeZIP = zip.value;
         dataInput.feelings = feelings.value;
 
+        queryAPI(dataInput.codeZIP, keyAPI).then((d) => {
+            dataInput.temp = d.main.temp;
+            save(dataInput, '/saveDate') // Send dataInput server side 
+                .then(getData()) // if run good => run getData()
+                .catch((err) => console.log(err));
+        });
 
-        queryWeather(dataInput, '/sendCodeZIP') // Send dataInput server side
-            .then(getData()) // if run good => run getData()
-            .catch((err) => console.log(err));
+
     } else {
         window.alert('not input data');
     }
